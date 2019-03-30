c3821397f9c83adc3197a6840bd7d5ce40cad16c
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-03-30 15:51:59 +0100

    Update NavBar look and added some custom css

--- src/features/nav/NavBar/NavBar.js
+++ src/features/nav/NavBar/NavBar.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { 
+import {
   Menu,
   Container,
   Button
- } from 'semantic-ui-react';
+} from 'semantic-ui-react';
 
 class NavBar extends Component {
   render() {
@@ -13,14 +13,14 @@ class NavBar extends Component {
           <Menu.Item header>
             <img src="assets/logo.png" alt="logo" />
             Re-vents
-                </Menu.Item>
+          </Menu.Item>
           <Menu.Item name="Events" />
           <Menu.Item>
-            <Button floated="right" positive inverted content="Create Event" />
+            <Button floated="right" inverted content="Create Event" />
           </Menu.Item>
           <Menu.Item position="right">
-            <Button basic inverted content="Login" />
-            <Button basic inverted content="Sign Out" style={{ marginLeft: '0.5em' }} />
+            <Button inverted content="Login" />
+            <Button inverted content="Sign Out" style={{ marginLeft: '0.5em' }} />
           </Menu.Item>
         </Container>
       </Menu>
