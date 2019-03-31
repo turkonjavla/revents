35a174b211b5fb1a10c76d1a301ba55b7f0eaa1a
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-03-31 12:41:17 +0200

    Map through events and send event prop to EventListItem

--- src/features/event/EventList/EventList.js
+++ src/features/event/EventList/EventList.js
@@ -3,11 +3,11 @@ import EventListItem from './EventListItem';
 
 class EventList extends Component {
   render() {
+    const { events } = this.props;
+    const eventList = events.map(event => <EventListItem key={event.id} event={event} />);
     return (
       <div>
-        <EventListItem />
-        <EventListItem />
-        <EventListItem />
+        { eventList }
       </div>
     )
   }
