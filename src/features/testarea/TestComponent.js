bf33bc288f89ef25351a1fff0650c90ccb18c7cb
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-04-04 16:14:23 +0200

    Add a simple increment/decrement counter to demonstrate react-redux

--- src/features/testarea/TestComponent.js
+++ src/features/testarea/TestComponent.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { incrementCounter, decrementCounte } from './testActions';
 
 class TestComponent extends Component {
   render() {
+    const { incrementCounter, decrementCounte, data } = this.props;
     return (
       <div>
         <h1>Test Area</h1>
+        <h3>The answer is: {data}</h3>
+        <Button onClick={incrementCounter} color="green" content="Increment" />
+        <Button onClick={decrementCounte} color="red" content="Decrement" />
       </div>
     )
   }
 }
 
-export default TestComponent;
+const mapStateToProps = state => ({
+  data: state.test.data
+})
+
+const actions = {
+  incrementCounter,
+  decrementCounte
+}
+
+export default connect(mapStateToProps, actions)(TestComponent);
