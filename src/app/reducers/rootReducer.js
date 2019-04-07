20d9bca4890d2725d391f1be90e165c2c72b7dc0
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-04-07 14:43:39 +0200

    Add redux-form reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as FormReducer} from 'redux-form'
 import testReducer from '../../features/testarea/testReducer';
 import eventRedcuer from '../../features/event/eventReducer';
 
 const rootReducer = combineReducers({
   test: testReducer,
-  events: eventRedcuer
+  events: eventRedcuer,
+  form: FormReducer
 });
 
 export default rootReducer;
