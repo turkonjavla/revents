90e40c38a9f3c042376b780f66d97203eafcc43d
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-04-19 14:06:52 +0200

    Load event details page for specific event

--- src/features/event/EventDetails/EventDetailsPage.js
+++ src/features/event/EventDetails/EventDetailsPage.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { withFirestore } from 'react-redux-firebase';
+import { toastr } from 'react-redux-toastr';
 import { Grid } from 'semantic-ui-react';
+import { objectToArray } from '../../../app/common/util/helpers';
 
 /* Components */
 import EventDetailsHeader from './EventDetailsHeader';
@@ -8,27 +11,41 @@ import EventDetailsInfo from './EventDetailsInfo';
 import EventDetailsChat from './EventDetailsChat';
 import EventDetailsSidebar from './EventDetailsSidebar';
 
-const EventDetailsPage = ({event}) => {
-  return (
-    <Grid>
-      <Grid.Column width={10}>
-        <EventDetailsHeader event={event} />
-        <EventDetailsInfo event={event} />
-        <EventDetailsChat />
-      </Grid.Column>
-      <Grid.Column width={6}>
-        <EventDetailsSidebar attendees={event.attendees} />
-      </Grid.Column>
-    </Grid>
-  )
+class EventDetailsPage extends Component {
+
+  async componentDidMount() {
+    const { firestore, match, history: { push } } = this.props;
+    let event = await firestore.get(`events/${match.params.id}`);
+
+    if (!event.exists) {
+      push('/events');
+      toastr.error('Sorry', 'Event not found');
+    }
+  }
+
+  render() {
+    const { event } = this.props;
+    const attendees = event && event.attendees && objectToArray(event.attendees);
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <EventDetailsHeader event={event} />
+          <EventDetailsInfo event={event} />
+          <EventDetailsChat />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <EventDetailsSidebar attendees={attendees} />
+        </Grid.Column>
+      </Grid>
+    )
+  }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  const eventId = ownProps.match.params.id;
+const mapStateToProps = (state) => {
   let event = {};
 
-  if(eventId && state.events.length > 0) {
-    event = state.events.filter(event => event.id === eventId)[0]
+  if (state.firestore.ordered.events && state.firestore.ordered.events[0]) {
+    event = state.firestore.ordered.events[0]
   }
 
   return {
@@ -36,6 +53,6 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(
+export default withFirestore(connect(
   mapStateToProps
-)(EventDetailsPage)
+)(EventDetailsPage));
