e3e7fd64e6ff864a1e75631e57006b022f0c31af
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-04-20 14:30:22 +0200

    Use objectToArray method for looping through attendees and add link to attendee profile

--- src/features/event/EventList/EventListAttendees.js
+++ src/features/event/EventList/EventListAttendees.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { List, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class EventListAttendees extends Component {
   render() {
     const { photoURL } = this.props.attendee;
     return (
       <List.Item>
-        <Image as="a" size="mini" circular src={ photoURL } />
+        <Image as={Link} to={`/profile/${this.props.attendee.id}`} size="mini" circular src={ photoURL } />
       </List.Item>
     )
   }
