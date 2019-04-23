e337c4e4f7ca1af6463969842667932df7bd9ce5
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-04-23 11:41:02 +0200

    Load events on scroll

--- src/features/event/EventList/EventList.js
+++ src/features/event/EventList/EventList.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import EventListItem from './EventListItem';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class EventList extends Component {
   render() {
-    const { events } = this.props;
-    const eventList = events && events.map(event => 
-      <EventListItem 
-        key={event.id} 
-        event={event} 
+    const { events, getNextEvents, loading, moreEvents } = this.props;
+    const eventList = events && events.map(event =>
+      <EventListItem
+        key={event.id}
+        event={event}
       />
     );
     return (
       <div>
-        { eventList }
+        {
+          events &&
+          events.length !== 0 &&
+          <InfiniteScroll 
+            pageStart={0}
+            loadMore={getNextEvents}
+            hasMore={!loading && moreEvents}
+            initialLoad={false}
+          >
+            {eventList}
+          </InfiniteScroll>
+        }
       </div>
     )
   }
